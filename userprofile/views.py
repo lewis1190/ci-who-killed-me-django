d8147785fb93c2ad28de0f807df7369fb55351f6
lewis1190 <lewiskos@hotmail.co.uk>
2026-01-19 18:23:50 +0000

    UserProfile now loads all reports belonging to the user. This will be paginated in the future.

--- userprofile/views.py
+++ userprofile/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
+from cheaterreports.models import CheaterPost
 
 # Create your views here.
 
@@ -21,8 +22,14 @@ def profile(request):
     else:
         form = UserProfileForm(instance=user_profile)
 
+    # Get all reports created by the logged-in user
+    reports = CheaterPost.objects.filter(author=request.user).order_by(
+        '-created_on'
+    )
+
     context = {
         'user_profile': user_profile,
         'form': form,
+        'reports': reports,
     }
     return render(request, 'userprofile/user_profile.html', context)
