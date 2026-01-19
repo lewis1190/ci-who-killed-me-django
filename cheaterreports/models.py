1d98e2130c9690329c1fc23707bdc5623bbc32b1
lewis1190 <lewiskos@hotmail.co.uk>
2026-01-19 23:29:09 +0000

    Real data of reports now being read on the home screen Additional media queries due to issues on small screens and tablets

--- cheaterreports/models.py
+++ cheaterreports/models.py
@@ -21,6 +21,7 @@ class CheaterPost(models.Model):
     youtube_url = models.URLField()
     suspected_hack_types = models.JSONField(default=list)
     description = models.TextField()
+    # score = models.IntegerField(default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from cheaterreports.models import CheaterPost
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home/home.html')
+    # Get the 6 most recent reports
+    recent_reports = CheaterPost.objects.all().order_by('-created_on')[:6]
+
+    context = {
+        'recent_reports': recent_reports,
+    }
+    return render(request, 'home/home.html', context)
