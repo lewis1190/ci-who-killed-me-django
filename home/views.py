99e441ee416c3f2e67113a938206850d542b87ea
lewis1190 <lewiskos@hotmail.co.uk>
2026-01-20 01:02:24 +0000

    Top clips in the home view now fetch highest scoring clips in last 30 days

--- home/views.py
+++ home/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
+from django.utils import timezone
+from datetime import timedelta
 from cheaterreports.models import CheaterPost
 
 # Create your views here.
 
 
 def home(request):
-    # Get the 6 most recent reports
-    recent_reports = CheaterPost.objects.all().order_by('-created_on')[:6]
+    # Get the 6 highest-scoring reports from the last 30 days
+    one_month_ago = timezone.now() - timedelta(days=30)
+    top_reports = CheaterPost.objects.filter(
+        created_on__gte=one_month_ago
+    ).order_by('-score')[:6]
 
     context = {
-        'recent_reports': recent_reports,
+        'recent_reports': top_reports,
     }
     return render(request, 'home/home.html', context)
