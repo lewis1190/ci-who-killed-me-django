ab0422ce475f1f6dc0811c33e742c8c415d13876
lewis1190 <lewiskos@hotmail.co.uk>
2026-01-21 00:52:32 +0000

    Created NewsPost model, ran the migration, registered the model in the Admin Panel

--- newsblog/admin.py
+++ newsblog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import NewsPost
 
 # Register your models here.
+
+
+@admin.register(NewsPost)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Lists fields for display in admin, fileds for search,
+    field filters, fields to prepopulate and rich-text editor.
+    """
+    list_display = ('title', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on',)
+    summernote_fields = ('content',)
--- newsblog/models.py
+++ newsblog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class NewsPost(models.Model):
+    """
+    Stores a single blog post entry related to :model:`auth.User`.
+    """
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="news_posts"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
